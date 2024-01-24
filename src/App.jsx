901f517765e83225e4cd6d4bb881e12afb2d730e
Anton <antonblazheiko@gmail.com>
2024-01-24 20:41:40 +0100

    :sparkles:initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import {Header} from './components/Header'
+
+import 'normalize.css';
+
+
+export const  App=() =>{
+
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import styles from './style.module.scss';
+
+
+export const Header = () => {
+  return (
+    <div className={styles.hello}>hello</div>
+  );
+}
+
+export default Header;
