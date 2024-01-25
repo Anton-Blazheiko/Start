0c291198282dacd4a6ca18584d249c1def0ce6c2
Viktor <yadelskyiviktor@gmail.com>
2024-01-25 13:33:00 +0200

    :sparkles:created custom input

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,14 @@
+
+import CicleIco from "../../img/svg/CircleIco"
+import styles from './style.module.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+
+ export const Input = ({placeholder, type='text' } ) => (
+    
+    <div className={cx('input')}>
+        <CicleIco/>
+        <input className={cx('input-text')}type={type} placeholder={placeholder} />
+    </div>
+ )
+
