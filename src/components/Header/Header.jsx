17fbe5ab00600f9aa711737b12cd10c1c1f255d3
Viktor <yadelskyiviktor@gmail.com>
2024-01-26 18:52:35 +0200

    :wrench:small styles fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-const menuItems = ['Articles', 'Locations', 'Sign in']
+const menuItems = ['Articles', 'Locations', 'Videos']
 
 
 export const Header = () => {
@@ -28,35 +28,36 @@ export const Header = () => {
   };
 
   return (
-    <header className={cx('header')}>
+    <>  
       <img  className={cx('header-intro-background')}src={IntroBackground}/>
-      <div className={cx(`${isSticky ? 'header-nav-container-is-sticky' : 'header-nav-container'}`)}>
-     
-      <Logo/>
+      <header className={cx('header')}>
+      
+        <div className={cx(`${isSticky ? 'header-nav-container-is-sticky' : 'header-nav-container'}`)}>
+      
+        <Logo/>
 
-      <nav className={cx(`header-nav`)}>
-        <ul className={cx('header-nav-ul')}>
-          {menuItems.map(item => {
-            return <li className={cx('header-nav-ul-li')} key={item}>{item}</li>
-          })}
-        </ul>
-      </nav>
-  
-    </div>
+        <nav className={cx(`header-nav`)}>
+          <ul className={cx('header-nav-ul')}>
+            {menuItems.map(item => {
+              return <li className={cx('header-nav-ul-li')} key={item}>{item}</li>
+            })}
+          </ul>
+        </nav>
     
-    <div className={cx('header-text')}>
-        <h1 className={cx('header-text-heading-title')}>Discover Amazing places in Japan</h1>
-        <p className={cx('header-text-heading-paragraph')}>Jump off balcony, onto strangers head. Chase ball of string hide when guests come over. Being gorgeous with belly side up i could pee on this if i had the energy but under the bed, for attack the child, open the door, </p>
-        
-    </div>
-
+        </div>
+      
+        <div className={cx('header-text')}>
+            <h1 className={cx('header-text-heading-title')}>Discover Amazing places in Japan</h1>
+            <p className={cx('header-text-heading-paragraph')}>Jump off balcony, onto strangers head. Chase ball of string hide when guests come over. Being gorgeous with belly side up i could pee on this if i had the energy but under the bed, for attack the child, open the door, </p>
+            
           <div className={cx('header-inputs')}>
-             <Input placeholder="What would you like to do?" />
-             <div className={cx('header-inputs-with-margin')}>
-                <Input placeholder="What would you like to do?" />
-             </div>
+            <Input placeholder="What would you like to do?" />
+            <div className={cx('header-inputs-with-margin')}>
+              <Input placeholder="What would you like to do?" />
+            </div>
           </div>
-       
-    </header>
+        </div>
+      </header>
+    </>
   );
 }
