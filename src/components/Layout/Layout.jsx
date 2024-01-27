6056f724e9d3381f485df5ec975824816853a13b
Viktor <yadelskyiviktor@gmail.com>
2024-01-27 12:14:17 +0200

    :sparkles:started Intro section

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,3 +1,6 @@
+
+
+import Intro from '../Intro';
 import styles from './style.module.scss';
 import classNames from 'classnames/bind';
 
@@ -5,7 +8,7 @@ const cx = classNames.bind(styles);
 
 export const Layout = () => (
     <main className={cx('main')}>
-        Layout component
+        <Intro />
     </main>
 )
 
